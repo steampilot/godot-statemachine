ece79260fc71b1762622df49938bfa8b45d8dc46
Steampilot <jerome.roeth@gmail.com>
2025-12-25 19:43:00 +0100

    fix: Update enemy slime green scene and script for improved damage handling and sprite reference

--- res/Scripts/enemy_slime_green.gd
+++ res/Scripts/enemy_slime_green.gd
@@ -2,13 +2,17 @@ extends Node2D
 @export var max_health = 100
 @export var health = max_health
 
+@onready var sprite: AnimatedSprite2D = %Sprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func receive_damage(amount: int) -> void:
-	print("%d received %d damage!", [% owner.name, amount])
+	print("%s received %d damage!" % [self.name, amount])
 	health -= amount
+	print("%s Is now at health: %d of %d" % [self.name, health, max_health])
+	sprite.play("hurt")
 	if health <= 0:
-		print("%d has been defeated!", [% owner.name])
+		print("%s has been defeated!" % [self.name])
 		queue_free()
