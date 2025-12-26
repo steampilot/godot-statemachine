05833ede46b6d108fbbe79949044c1626b3e7138
Steampilot <jerome.roeth@gmail.com>
2025-12-26 19:33:51 +0100

    fix: Move player properties to the top for better organization

--- res/Scripts/player.gd
+++ res/Scripts/player.gd
@@ -3,6 +3,13 @@ extends CharacterBody2D
 
 @export var max_health: int = 100
 
+@export var down_gravity_multiplier: float = 1.5
+@export var max_speed: float = 400.0
+@export var acceleration: float = 2000.0
+@export var deceleration: float = 1500.0
+@export var turn_speed: float = 1.5
+
+
 var health: int = max_health
 
 @onready var sprite: AnimatedSprite2D = %Sprite
